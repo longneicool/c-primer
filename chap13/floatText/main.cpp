4bf29d3d4c2d58abc67e6d2d1d439a74e10afbe6
zk <z;@zk.com>
2016-03-05 20:57:49 +0800

    add floatText

--- chap13/floatText/main.cpp
+++ chap13/floatText/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int count = 0;
+    while(1)
+    {
+        double d = double(30/10.0);
+        if(3 != int(d))
+        {
+            cout << "the int unequal float!" << endl;
+            break;
+        }
+
+        cout << count++ << endl;
+    }
+
     return 0;
 }
 
